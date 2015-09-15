9f3a3a62ff6e1157ed28a3065bfbe864cd3a237d
Kelvin Lu <kelvin.lu.sj@gmail.com>
2015-09-14 17:58:54 -0700

    first working routes for showing contributors and offers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -61,6 +61,10 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def contributors
+    @contributors = Project.find(params[:id]).users
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
