f1031830e212099e9954f58232a398d721dcef62
Mike Ryan <flyinmryan@gmail.com>
2015-09-17 13:33:56 -0700

    Seeded DB with Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,22 @@
 # Examples:
 # #
 #   users = User.create([{ name: 'Ji' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+  User.create(name: "Bill Nye", email: "bn@gmail.com", password: "123", role:
+"Developer", title: "Full Stack Developer", description: "I used to teach science, now I write code", rating: 43, total_ratings:
+ 10, photo: "men/18")
+
+ User.create(name: "Neil Tyson", email: "nt@gmail.com", password: "123", role:
+ "Developer", title: "Database Minimalization", description: "I concentrate on compression.  The same DB can store a lot more information if files are compressed", rating: 48, total_ratings:
+ 10, photo: "men/13")
+
+ User.create(name: "Carl Seagram", email: "cs@gmail.com", password: "123", role:
+"Developer", title: "Front End Angular", description: "Focused on User Interface and User Experience.  I have experience with many different front end frameworks.", rating: 34, total_ratings:
+10, photo: "men/14")
+
+User.create(name: "Sunshine Construction", email: "sc@gmail.com", password: "123", role:
+"Client", title: "Sunshine Contruction", description: "We do everything from filling pot holes to constructing entire community centers", rating: 34, total_ratings:
+10, photo: "men/15")
+
+User.create(name: "Safeway", email: "s@gmail.com", password: "123", role:
+"Client", title: "Safeway", description: "We do neighborhood groceries", rating: 24, total_ratings:
+10, photo: "women/15")
